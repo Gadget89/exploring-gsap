8a68aff11d57ce3284c548abec54c06a862f9ca0
Nicolas Roybal <nicolas.roybal@hotmail.com>
2017-10-01 21:34:20 -0500

    Added JSON data for nav buttons

--- src/Search.js
+++ src/Search.js
@@ -2,6 +2,38 @@ import React from 'react';
 import './Main.css';
 import {TweenMax} from "gsap";
 
+const navItem = [
+  {
+    id: "about",
+    name: "my story",
+    link: "#about",
+    title: "Check out my story."
+  },
+  {
+    id: "skills",
+    name: "skills",
+    link: "#skills",
+    title: "Check out my skills."
+  },
+  {
+    id: "projects",
+    name: "projects",
+    link: "#projects",
+    title: "Check out my projects"
+  },
+  {
+    id: "contact",
+    name: "contact me",
+    link: "#contact",
+    title: "Let's get in touch"
+  },
+  {
+    id: "blog",
+    link: "https://medium.com/@nicolas.roybal",
+    name: "blog",
+    title: "Check out my blog"
+  }
+]
 
 class HelloMessage extends React.Component {
  componentDidMount() {
@@ -10,6 +42,12 @@ class HelloMessage extends React.Component {
 
  render() {
   return <div id="box" className="hello">Hello {this.props.name}</div>;
+    <nav id=lg-screen>
+      <ul>
+        <li id="box">li 1</li>
+        <li id="box">li 2</li>
+      </ul>
+    </nav>
  }
 }
 
